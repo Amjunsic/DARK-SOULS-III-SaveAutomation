4aeefe96ad1f5eaa557cba8e55f7c36ecc9a8ab7
DESKTOP-CGD8L5N\rlawo <rlawodmsajtw0@gmail.com>
2021-09-09 20:40:09 +0900

    Program Gui
    title image change
    title name change(DARK SOULS™ III SaveAutomation)

--- practice.py
+++ practice.py
@@ -1,1 +1,22 @@
-print("hello world")
+from tkinter import *
+import tkinter
+
+class AppGui(tkinter.Tk):
+
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
+        
+        self.parent = parent
+        self.initialize()
+    
+    def initialize(self):
+        pass
+
+root = AppGui(NONE)
+root.title("DARK SOULS™ III SaveAutomation")
+root.geometry("600x300")
+root.minsize(600,300)
+root.maxsize(900,600)
+root.call('wm', 'iconphoto', root._w,PhotoImage(file='image.ico'))
+root.iconphoto(True, PhotoImage(file='image.ico'))
+root.mainloop()
