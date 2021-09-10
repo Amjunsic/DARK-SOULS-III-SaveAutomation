1d3d3cd0ec5dd31698c692fe695c54d531e27228
DESKTOP-CGD8L5N\rlawo <rlawodmsajtw0@gmail.com>
2021-09-10 13:52:45 +0900

    Code Style Change

--- practice.py
+++ practice.py
@@ -1,21 +1,18 @@
+from os import name
 from tkinter import *
 import tkinter
 
 class AppGui(tkinter.Tk):
 
-    def __init__(self, parent):
-        tkinter.Tk.__init__(self, parent)
-        
-        self.parent = parent
-        self.initialize()
-    
-    def initialize(self):
-        self.title("DARK SOULS™ III SaveAutomation")
-        self.geometry("600x300")
-        self.minsize(600,300)
-        self.maxsize(900,600)
-        self.call('wm', 'iconphoto', self._w,PhotoImage(file='image.ico'))
-        self.iconphoto(True, PhotoImage(file='image.ico'))
+    def __init__(self):
+            self.root = Tk()
+            self.root.title("DARK SOULS™ III SaveAutomation")
+            self.root.geometry("600x300")
+            self.root.minsize(600,300)
+            self.root.maxsize(900,600)
+            self.root.call('wm', 'iconphoto', self._w,PhotoImage(file='image.ico'))
+            self.root.iconphoto(True, PhotoImage(file='image.ico'))
+            self.root.mainloop()
 
-root = AppGui(NONE)
-root.mainloop()
+if __name__ == '__main__':
+    root = AppGui()
